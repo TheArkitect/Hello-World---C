ade0eb3adee08a1ee3a46245a6cadff77151213e
kishan <kishan.sondagar@gmail.com>
2017-11-08 13:28:01 -0500

    added

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-
-int main(){
-	printf("Hello Github!\n");
+#include <stdio.h>				/*including info about the standarrd library*/
+	
+int main()					/*defined function named main with no arg values*/
+{						/*braces enclose the statements to be executed*/
+	printf("Hello Github!\n");		/*printf will display hello github, \n will call nextline*/
 }
